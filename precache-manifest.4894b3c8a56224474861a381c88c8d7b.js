c7b7e7f56775f5591b3068aeb93900c9d91cc925
Mananx23 <manan.olympian23@gmail.com>
2020-09-09 13:21:53 +0530

    Updates

---
+++ precache-manifest.4894b3c8a56224474861a381c88c8d7b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2719fc095f10a94876de764e2bec1b7f",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "5baadcadbc5a90138cef",
+    "url": "/Portfolio/static/css/main.173dd690.chunk.css"
+  },
+  {
+    "revision": "e6ed7b6c59f337ff7a5d",
+    "url": "/Portfolio/static/js/2.e6404c50.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio/static/js/2.e6404c50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5baadcadbc5a90138cef",
+    "url": "/Portfolio/static/js/main.f7e9df32.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  },
+  {
+    "revision": "c05c81617c94d753e9dcbb1c21e59126",
+    "url": "/Portfolio/static/media/Crypton.c05c8161.PNG"
+  },
+  {
+    "revision": "8de016d0e626be7833ed93930e371c82",
+    "url": "/Portfolio/static/media/Netflix.8de016d0.PNG"
+  },
+  {
+    "revision": "7f487cc28cb38f659a1fd13d5b233381",
+    "url": "/Portfolio/static/media/Rick.7f487cc2.PNG"
+  },
+  {
+    "revision": "077ee686085a26ae5511f8dea0fab0d8",
+    "url": "/Portfolio/static/media/manan.077ee686.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Portfolio/precache-manifest.d397455fd716243f9d126a3de007a4de.js"
+  "/Portfolio/precache-manifest.4894b3c8a56224474861a381c88c8d7b.js"
 );
 
 self.addEventListener('message', (event) => {
